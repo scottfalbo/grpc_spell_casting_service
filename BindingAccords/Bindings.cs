b801bd9baec8677bb25e0326775b723954462f4e
Scott Falbo <scottfalboart@gmail.com>
2023-03-23 20:40:38 -0700

    added service contracts a proto schemas

---
+++ BindingAccords/Bindings.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace BindingAccords
+{
+    [DataContract]
+    public class Bindings
+    {
+        public class BundledScrolls
+        {
+            [DataMember(Order = 1)]
+            public List<RequestScroll> Scrolls { get; set; }
+
+            [DataMember(Order = 2)]
+            public CastingStatus Status { get; set; }
+        }
+
+        public class RequestScroll
+        {
+            [DataMember(Order = 1)]
+            public string UniqueGlyph { get; set; }
+
+            [DataMember(Order = 2)]
+            public string CastingPhrase { get; set; }
+
+            [DataMember(Order = 3)]
+            public int MagicType { get; set; }
+
+            [DataMember(Order = 4)]
+            public int SpellType { get; set; }
+        }
+
+        public class ResponseStatus
+        {
+            [DataMember(Order = 1)]
+            public CastingStatus Status { get; set; }
+
+            [DataMember(Order = 2)]
+            public string Message { get; set; }
+        }
+
+        public enum CastingStatus
+        {
+            Success = 0,
+            Failure = 1,
+            Unknown = 2
+        }
+    }
+}
---
+++ BindingAccords/ICastingService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using System.ServiceModel;
+
+namespace BindingAccords
+{
+    [ServiceContract]
+    public interface ICastingService
+    {
+        [OperationContract]
+        Task<ResponseStatus> Cast(Bindings request, ServerCallContext context);
+    }
+}
